ac4a991acbd65af30b9f5459b97c356f726384a5
Rogudux <diegorojas13579@gmail.com>
2024-08-19 09:18:58 -0600

    19/08/24

---
+++ prac_01/js/prac_01.js
@@ -0,0 +1,3 @@
+//existe var y let pero se sigue usan mas let que var LET FAV
+var mensaje = "hola critoher";
+console.log(mensaje);
---
+++ prac_01/scr/prac_01.ts
@@ -0,0 +1,6 @@
+//existe var y let pero se sigue usan mas let que var LET FAV
+let mensaje:string = "hola critoher"
+console.log(mensaje)
+
+let numero:number = 3
+console.log(numero)
