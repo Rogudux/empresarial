77face3b9ad342d30030a11a561c3c41dcfc6e1c
Rogudux <diegorojas13579@gmail.com>
2024-09-10 13:22:28 -0600

    Practica 5 y 6

--- prac_05/src/index.ts
+++ prac_05/src/index.ts
@@ -11,3 +11,33 @@ console.log(first);
 let nombre = "Diego";
 let [a1,a2,a3,a4,a5]:any= nombre;
 console.log (a1,a2,a3,a4,a5);
+
+class Persona{
+    nombre:string;
+    edades:number;
+    constructor(nombre:string, edades:number){
+        this.nombre = nombre;
+        this.edades = edades;
+    }
+    getEdad():number{
+        return this.edades;
+    }
+    setEdad(edades:number){
+        this.edades = edades; 
+    }
+
+    getNombre():string{
+        return this.nombre;
+    }
+    setNombre(nombre:string){
+        this.nombre = nombre;
+    }
+}
+let juan = new Persona('Juan', 30);
+console.log(juan);
+console.log(juan.nombre);
+juan.nombre = "Pedro"
+juan.setEdad(23);
+
+console.log("Nombre:",  juan.getNombre());
+console.log("Edad:",  juan.getEdad());
